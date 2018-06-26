91bc661157019e7418d61a00132a214e2c19920f
Burak <burakkiran95@gmail.com>
2018-06-25 22:24:50 -0700

    added reac-router to app. Created basic navigation bar. set up initial routing. Feel free to change routing from test component to new componet that are created.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar.jsx';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Test from './Test.jsx'
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">New Project</h1>
-        </header>
-        <p className="App-intro">
-          This is going to be a dank new app.
-        </p>
+          <NavBar/>
+          <Route path="/home" component={Test} />
+          <Route path="/archives" component={Test} />
+          <Route path="/features" component={Test} />
+          <Route path="/contact" component={Test} />
+          <Route path="/about" component={Test} />
       </div>
+      </Router>
     );
   }
 }
